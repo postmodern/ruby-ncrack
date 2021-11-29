614965802904c752703e0d40f351328030d0df04
Postmodern <postmodern.mod3@gmail.com>
2021-11-29 05:32:23 -0800

    Use RSpec 3.x syntax.

--- spec/ncrack_spec.rb
+++ spec/ncrack_spec.rb
@@ -3,6 +3,6 @@ require 'ncrack'
 
 describe Ncrack do
   it "should have a VERSION constant" do
-    subject.const_get('VERSION').should_not be_empty
+    expect(subject.const_get('VERSION')).to_not be_empty
   end
 end
